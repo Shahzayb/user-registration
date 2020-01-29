2e48e9bf3ca0453cd615edd3a33f8d26844c1a43
Shahzaib Sarwar <imshahzayb@gmail.com>
2020-01-30 04:40:06 +0500

    added get /api/user route tests

--- server/tests/user.test.js
+++ server/tests/user.test.js
@@ -7,67 +7,157 @@ const { createToken } = require('../util/jwt');
 
 const req = supertest(app);
 
-test('user can register', async done => {
-  const userObj = {
-    username: 'Shah',
-    name: 'shahZaib',
-    email: 'imshahzayb@gmail.com',
-    password: '12345678'
-  };
+describe('GET /api/user', () => {
+  test('search users with pagination', async done => {
+    const user1 = new User({
+      username: 'shah1',
+      name: 'shahzaib',
+      email: 'im1@gmail.com',
+      password: 'dummy hash password'
+    });
+    const user2 = new User({
+      username: 'shah2',
+      name: 'shahzaib2',
+      email: 'im2@gmail.com',
+      password: 'dummy hash password 2'
+    });
 
-  const res = await req
-    .post('/api/user')
-    .set('Content-Type', 'application/json')
-    .send(userObj);
+    await user1.save();
+    await user2.save();
 
-  const user = await User.findById(res.body.user._id, { username: 1 }).lean();
+    const res = await req
+      .get('/api/user')
+      .query({ page: 1 })
+      .query({ size: 1 })
+      .query({ q: user1.username });
 
-  expect(user.username).toEqual(res.body.user.username);
-  done();
+    console.log(res.body);
+
+    expect(res.body[0].username).toBe(user1.username);
+
+    done();
+  });
+
+  test('invalid page & size query params are not accepted', async done => {
+    const res1 = await req
+      .get('/api/user')
+      .query({ page: 0 })
+      .query({ size: 1 });
+
+    const res2 = await req
+      .get('/api/user')
+      .query({ page: 2 })
+      .query({ size: 1000 });
+
+    expect(res1.status).toBe(422);
+    expect(res2.status).toBe(422);
+    done();
+  });
+
+  test('can get list of all users ( with pagination, and without search query )', async done => {
+    const user1 = new User({
+      username: 'shah1',
+      name: 'shahzaib',
+      email: 'im1@gmail.com',
+      password: 'dummy hash password'
+    });
+    const user2 = new User({
+      username: 'shah2',
+      name: 'shahzaib2',
+      email: 'im2@gmail.com',
+      password: 'dummy hash password 2'
+    });
+
+    await user1.save();
+    await user2.save();
+
+    const res1 = await req
+      .get('/api/user')
+      .query({ page: 1 })
+      .query({ size: 1 });
+
+    const res2 = await req
+      .get('/api/user')
+      .query({ page: 2 })
+      .query({ size: 1 });
+
+    const res3 = await req
+      .get('/api/user')
+      .query({ page: 3 })
+      .query({ size: 1 });
+
+    expect(res1.body[0].username).toBe(user2.username);
+    expect(res2.body[0].username).toBe(user1.username);
+    expect(res3.body.length).toBe(0);
+    done();
+  });
 });
 
-test('user can login', async done => {
-  const userObj = {
-    username: 'shah',
-    name: 'shahzaib',
-    email: 'imshahzayb@gmail.com',
-    password: '12345678'
-  };
+describe('POST /api/user/login', () => {
+  test('user can login', async done => {
+    const userObj = {
+      username: 'shah',
+      name: 'shahzaib',
+      email: 'imshahzayb@gmail.com',
+      password: '12345678'
+    };
 
-  const hashPass = await bcrypt.hash(userObj.password, 8);
+    const hashPass = await bcrypt.hash(userObj.password, 8);
 
-  await User.create({ ...userObj, password: hashPass });
+    await User.create({ ...userObj, password: hashPass });
 
-  const res = await req
-    .post('/api/user/login')
-    .set('Content-Type', 'application/json')
-    .send({ username: userObj.username, password: userObj.password });
+    const res = await req
+      .post('/api/user/login')
+      .set('Content-Type', 'application/json')
+      .send({ username: userObj.username, password: userObj.password });
 
-  const jwtToken = await createToken({ username: res.body.user.username });
+    const jwtToken = await createToken({ username: res.body.user.username });
 
-  expect(jwtToken).toEqual(res.body.token);
+    expect(jwtToken).toEqual(res.body.token);
 
-  done();
+    done();
+  });
+
+  test('user cannot login with invalid username or password', async done => {
+    const userObj = {
+      username: 'shah',
+      name: 'shahzaib',
+      email: 'imshahzayb@gmail.com',
+      password: '12345678'
+    };
+
+    const hashPass = await bcrypt.hash(userObj.password, 8);
+
+    await User.create({ ...userObj, password: hashPass });
+
+    const res = await req
+      .post('/api/user/login')
+      .set('Content-Type', 'application/json')
+      .send({ username: userObj.username, password: 'invalid' });
+
+    expect(res.status).toEqual(401);
+
+    done();
+  });
 });
 
-test('user cannot login with invalid username or password', async done => {
-  const userObj = {
-    username: 'shah',
-    name: 'shahzaib',
-    email: 'imshahzayb@gmail.com',
-    password: '12345678'
-  };
+describe('POST /api/user', () => {
+  test('user can register', async done => {
+    const userObj = {
+      username: 'Shah',
+      name: 'shahZaib',
+      email: 'imshahzayb@gmail.com',
+      password: '12345678'
+    };
 
-  const hashPass = await bcrypt.hash(userObj.password, 8);
+    const res = await req
+      .post('/api/user')
+      .set('Content-Type', 'application/json')
+      .send(userObj);
 
-  await User.create({ ...userObj, password: hashPass });
+    const user = await User.findById(res.body.user._id, { username: 1 }).lean();
 
-  const res = await req
-    .post('/api/user/login')
-    .set('Content-Type', 'application/json')
-    .send({ username: userObj.username, password: 'invalid' });
-
-  expect(res.status).toEqual(401);
-
-  done();
+    expect(user.username).toEqual(res.body.user.username);
+    done();
+  });
 });
